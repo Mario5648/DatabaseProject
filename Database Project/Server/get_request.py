4bbfcdeea37ba5e8b699cde1134273fe2b19cb46
Mario5648 <65876827+Mario5648@users.noreply.github.com>
2020-11-03 23:39:25 -0600

    Add files via upload

--- Database Project/Server/get_request.py
+++ Database Project/Server/get_request.py
@@ -9,7 +9,7 @@ dictionary_to_json
 
 This will take a dictionary
 and it will transform it to
-JSON data. 
+JSON data.
 
 """
 def dictionary_to_json(JSONdictionary):
@@ -39,6 +39,36 @@ def get(DB_request):
         conn.commit()
         conn.close()
 
+
+        #update values from the retrived results
+        #in range of the length of the response to prevent errors with NULL entries
+        for i in range(len(DB_response)):
+            json_response[i]["img_url"] = DB_response[i][4]
+            json_response[i]["product_title"] = DB_response[i][8]
+            json_response[i]["price"] = DB_response[i][3]
+
+        #call method to convert the dictionary to JSON
+        json_response = dictionary_to_json(json_response)
+        return json_response
+        
+    elif DB_request == "JUSTADD":
+        #Create an empty dictionary to contain all objects with their attributes
+        json_response = {0:{},1:{},2:{},3:{},4:{},5:{},6:{},7:{}}
+
+        #create each dictionary for item
+        for i in range(8):
+            json_response[i]["img_url"] = ""
+            json_response[i]["product_title"] = ""
+            json_response[i]["price"] = 0.0
+
+        #Make request to the datbase file and acess the most recent
+        conn = sqlite3.connect('product.db')
+        c = conn.cursor()
+        #This will order from most recent to oldest
+        c.execute("SELECT * FROM product ORDER BY Product_ID DESC")
+        DB_response = c.fetchmany(8)
+        conn.commit()
+        conn.close()
 
         #update values from the retrived results
         #in range of the length of the response to prevent errors with NULL entries
